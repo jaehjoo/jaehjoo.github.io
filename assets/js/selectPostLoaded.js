2ed7f0320db932a1e3f6720322b417f83a67a439
Jae-Ho Joo <jae-hojoo@Jae-Houi-MacBookAir.local>
2024-10-05 20:27:29 +0900

    add state

--- assets/js/selectPostLoaded.js
+++ assets/js/selectPostLoaded.js
@@ -1,8 +1,13 @@
-function loadPost(title, content) {
+function loadPost(title, content, newUrl) {
     document.querySelector("#content-frame").innerHTML = `
         <div class="single-post">
             <h1>${title}</h1>
             <div>${content}</div>
         </div>
     `;
+    window.history.pushState({title, content}, title, newUrl);
 }
+
+window.onpopstate = () => {
+    window.location.href = '/';
+};
